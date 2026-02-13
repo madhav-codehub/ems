d11a941cec2372eb85927a8f4025bda3a79c52d0
madhav-codehub <madhav.p.akv@gmail.com>
2026-02-13 18:38:41 +0530

    update

--- src/main/java/com/luv4code/employeemanagementsystem/controller/EmployeeController.java
+++ src/main/java/com/luv4code/employeemanagementsystem/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.luv4code.employeemanagementsystem.controller;
 import com.luv4code.employeemanagementsystem.entity.Employee;
 import com.luv4code.employeemanagementsystem.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +15,14 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/employees")
+@Slf4j
 public class EmployeeController {
 
     private final EmployeeService employeeService;
 
     @GetMapping
     public ResponseEntity<List<Employee>> getEmployeeList(){
+        log.info("List the all employees....");
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(employeeService.getAllEmployees());
