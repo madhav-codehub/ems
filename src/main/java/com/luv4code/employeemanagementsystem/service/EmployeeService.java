75ea3544178d1ff7c3630e2f3b18678a4847b4b1
madhav-codehub <madhav.p.akv@gmail.com>
2026-02-13 18:40:49 +0530

    update

--- src/main/java/com/luv4code/employeemanagementsystem/service/EmployeeService.java
+++ src/main/java/com/luv4code/employeemanagementsystem/service/EmployeeService.java
@@ -3,17 +3,20 @@ package com.luv4code.employeemanagementsystem.service;
 import com.luv4code.employeemanagementsystem.entity.Employee;
 import com.luv4code.employeemanagementsystem.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
     public List<Employee> getAllEmployees(){
+        log.info("starting the service method");
         return employeeRepository.findAll();
     }
 
